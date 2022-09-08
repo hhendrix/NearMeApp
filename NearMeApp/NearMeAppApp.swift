18e8bcd684ca9e4e6f72cc44f98de4fa00555bf2
Harry Lopez <harry.lopez@overactive.com>
2022-09-08 09:39:52 -0500

    Initial Commit

---
+++ NearMeApp/NearMeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NearMeAppApp.swift
+//  NearMeApp
+//
+//  Created by Harry Lopez on 8/09/22.
+//
+
+import SwiftUI
+
+@main
+struct NearMeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
